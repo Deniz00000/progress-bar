7d9e59a9e8e7f0a4cf2b10ff2cc220fac8d898e5
Deniz00000 <92266788+Deniz00000@users.noreply.github.com>
2021-11-04 12:13:05 +0300

    Add files via upload

---
+++ progress bar/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progress_bar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+            int cemil = Convert.ToInt32(textBox1.Text);
+            progressBar1.Value = cemil;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("hata:"+ex.Message);
+               
+            }
+
+
+        }
+    }
+}
